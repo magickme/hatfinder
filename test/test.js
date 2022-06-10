5782496d7e90f383da246a83cb509fc70900eb3e
Jason Louv <jason@ultraculture.org>
2022-06-09 23:40:02 -0500

    Added Chai

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+
 describe('Array', () => {
     describe('#indexOf()', () => {
         it('should return -1 when the value is not present', () => {
@@ -6,3 +7,21 @@ describe('Array', () => {
         })
     })
 });
+
+describe('establish 2D array representing field', () => {
+            it('defines Field constructor', () => {
+                class Field {
+                    constructor(field) {
+                        this.field = myField;
+                    }
+                };
+                assert.equal
+                    /*it('Array object is created', () => {
+                        const myField = new Field([
+                            ['*', '░', 'O'],
+                            ['░', 'O', '░'],
+                            ['░', '^', '░']
+                        ]);
+                        assert.equal(myField, ['*', '░', 'O'], ['░', 'O', '░'], ['░', '^', '░']);
+                    })*/
+            });
